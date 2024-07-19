566a1c7f01a4dbc7893935dfca51b18b11690572
eum2o <msc-gh.eum2o@aleeas.com>
2024-07-19 19:26:58 +0200

    read path from args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+const EXEC_NAME: &str = "ftree";
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().take(2).collect();
+    if args.len() != 2 {
+        panic!("Error: Invalid arguments.\nSyntax: {EXEC_NAME} <path>\nExample: {EXEC_NAME} .");
+    }
+
+    let path = &args[1];
+
+    println!("Path {}", &path);
 }
